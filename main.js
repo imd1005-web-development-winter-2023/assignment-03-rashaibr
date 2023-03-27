1e03b1bc68e62c8b817488734c9fd3c0e7d261d1
rashaibr <96967976+rashaibr@users.noreply.github.com>
2023-03-26 21:15:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,85 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+ 
+//selectors
+const todoInput = document.querySelector('.add-todo');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
+const categoryOption = document.querySelector(".category-option");
+//event listeners
 
-//
-// Variables
-//
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+categoryOption.addEventListener('click'.categoryOption);
+//functions
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// Variables
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+function addTodo(event){
+  //prevent from submitting form
+  event.preventDefault(); 
+  
+  //todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+//create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //check trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //append to the list
+  todoList.appendChild(todoDiv);
+  //clear todo input value
+  todoInput.value = "";
 }
+function deleteCheck(e){
+  const item = e.target;
+  if (item.classList[0] == "trash-btn"){
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  } 
+  //cehck mark
+  if (item.classList[0] == "complete-btn"){
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  } 
+}
+function filterTodo(e){
+  const todos = todoList.querySelectorAll(".todo");
+  todos.forEach(function(todo){
+      switch (e.target.value){
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case"completed":
+          if(todo.classList.contains('completed')){
+            todo.style.display = "flex";
+          }
+          else{
+            todo.style.display ="none";
+        }
+        break;
+        case "uncompleted":
+          if (!todo.classList.contains('completed')){
+            todo.style.display = "flex";
+          }
+          else{
+            todo.style.display ="none";
 
-//
-// Inits & Event Listeners
-//
-
-inititialise();
+          }
+         break; 
+      }
+  });
+  
+}
